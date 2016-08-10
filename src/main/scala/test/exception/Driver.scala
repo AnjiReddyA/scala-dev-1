cf9550beb3241c3d01cb2c485d7936dd4b08b38d
edureka <edureka@localhost.localdomain>
2016-08-10 12:07:58 +0530

    restructured files

---
+++ src/main/scala/test/exception/Driver.scala
@@ -0,0 +1,27 @@
+package test.exception
+
+import scala.util.{Try, Success, Failure}
+
+class TestException {
+	def findElementIndex(e: Int, list: List[Int]): Int = {
+		
+		val index: Int = list.indexOf(e, 0)
+		
+		if (index == -1)
+			throw new Exception("Element not found.")
+		index
+	}
+}
+
+object Driver {
+	def main(args: Array[String]): Unit = {
+	  
+		val list = List(1, 2, 3, 4, 5)
+		
+		Try(new TestException().findElementIndex(3, list)) match {
+			
+			case Success(index) => println(s"Found element at index = $index")
+			case Failure(exception) => println(exception.getMessage)
+		}
+	}
+}
---
+++ src/main/scala/test/hof/GenericOp.scala
@@ -0,0 +1,34 @@
+package test.hof
+
+/**
+ * @author edureka
+ */
+object GenericOp {
+
+	def genericOp[T <: Any](f: (T, T) => T, op1: T, op2: T): T = {
+		f(op1, op2)
+	}
+
+	def op(f: (Int, Int) => Int, op1: Int, op2: Int): Int = {
+		f(op1, op2)
+	}
+
+	def multiply(x: Int, y: Int): Int = {
+		x * y
+	}
+
+	def main(args: Array[String]): Unit = {
+
+		// calling by name
+		println(op(multiply, 2, 3))
+
+		// using Function Literals/Anonymous Functions/Lambda functions
+		println(op((x, y) => x * y, 2, 3))
+
+		// calling with any numeric types
+		println(genericOp[Float]((x, y) => x * y, 2.5f, .3f))
+		// alternative syntax
+		println(genericOp((x: Int, y: Int) => x * y, 2, 3))
+	}
+
+}
